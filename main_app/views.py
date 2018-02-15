390061be124d093b88c23f32a3c5e5cdbc672269
Lillian Chernin <l.smith1654@gmail.com>
2018-02-15 11:26:33 -0600

    finish part1 of Treasuregram

---
+++ main_app/views.py
@@ -0,0 +1,8 @@
+# main_app/views.py
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Treasure
+
+def index(request):
+    treasures = Treasure.objects.all()
+    return render(request, 'index.html', {'treasures': treasures})
